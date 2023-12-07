439a2dd4343af3f817c2aeb5bb91b0d2f0a76bad
sanderbolt050 <52425276+sanderbolt050@users.noreply.github.com>
2023-12-07 14:15:41 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -21,22 +21,26 @@ calculate_hash() {
     md5sum "$1" | awk '{print $1}'
 }
 
-# Functie om foto's te kopiëren en controleren op succesvolle kopie
-copy_and_verify() {
+# Functie om foto's te verplaatsen en controleren op succesvolle verplaatsing
+move_and_verify() {
     source_file="$1"
     destination="$2"
 
-    # Kopieer het bestand naar de bestemming
-    cp "$source_file" "$destination"
+    # Verplaats het bestand naar de bestemming
+    mv "$source_file" "$destination"
 
-    # Controleer of de kopie succesvol was
+    # Controleer of de verplaatsing succesvol was
     if [ $? -eq 0 ]; then
-        echo "Kopie succesvol: $source_file -> $destination"
+        echo "Verplaatsing succesvol: $source_file -> $destination"
     else
-        echo "Fout bij kopiëren: $source_file"
+        echo "Fout bij verplaatsen: $source_file"
         exit 1
     fi
 }
+
+# Variabelen voor originele en gekopieerde foto's
+original_photo=""
+copied_photo=""
 
 # Organiseer foto's op basis van de opgegeven optie (maand/week)
 case "$organize_option" in
@@ -44,48 +48,20 @@ case "$organize_option" in
         # Verplaats foto's naar de juiste map op basis van de maand
         for photo in "$photo_directory"/*; do
             month=$(date -r "$photo" +"%m")
-            
-            if [ ! -d "$photo_directory/$month" ]; then
-                mkdir -p "$photo_directory/$month"
-            fi
-
-            copy_and_verify "$photo" "$photo_directory/$month/"
-            
-            # Verwijder origineel als de kopie succesvol is
-            original_hash=$(calculate_hash "$photo")
+            mkdir -p "$photo_directory/$month"
+            move_and_verify "$photo" "$photo_directory/$month/"
+            original_photo="$photo"
             copied_photo="$photo_directory/$month/$(basename "$photo")"
-            copy_hash=$(calculate_hash "$copied_photo")
-
-            if [ "$original_hash" == "$copy_hash" ]; then
-                rm "$photo"
-                echo "Originele foto verwijderd: $photo"
-            else
-                echo "Fout bij het verwijderen van originele foto: $photo"
-            fi
         done
         ;;
     "week")
         # Verplaats foto's naar de juiste map op basis van de week
         for photo in "$photo_directory"/*; do
             week=$(date -r "$photo" +"%U")
-            
-            if [ ! -d "$photo_directory/week_$week" ]; then
-                mkdir -p "$photo_directory/week_$week"
-            fi
-
-            copy_and_verify "$photo" "$photo_directory/week_$week/"
-            
-            # Verwijder origineel als de kopie succesvol is
-            original_hash=$(calculate_hash "$photo")
+            mkdir -p "$photo_directory/week_$week"
+            move_and_verify "$photo" "$photo_directory/week_$week/"
+            original_photo="$photo"
             copied_photo="$photo_directory/week_$week/$(basename "$photo")"
-            copy_hash=$(calculate_hash "$copied_photo")
-
-            if [ "$original_hash" == "$copy_hash" ]; then
-                rm "$photo"
-                echo "Originele foto verwijderd: $photo"
-            else
-                echo "Fout bij het verwijderen van originele foto: $photo"
-            fi
         done
         ;;
     *)
@@ -93,5 +69,16 @@ case "$organize_option" in
         exit 1
         ;;
 esac
+
+# Verwijder originele foto als de kopie succesvol is
+original_hash=$(calculate_hash "$original_photo")
+copy_hash=$(calculate_hash "$copied_photo")
+
+if [ "$original_hash" == "$copy_hash" ]; then
+    rm "$original_photo"
+    echo "Originele foto verwijderd: $original_photo"
+else
+    echo "Fout bij het verwijderen van originele foto: $original_photo"
+fi
 
 echo "Organisatie voltooid."
